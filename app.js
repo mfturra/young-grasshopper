bc1cd020292c2262553717c3c4010dc82a64e018
Matheus Turra <turra.m@northeastern.edu>
2023-09-19 17:58:43 -0400

    Fixed issue with output not appearing

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
+// Holistic calculate function
 window.onload = () =>
-    //the function called when Calculate button is clicked.
     {
-        /*calling a function calculateTip
-         which will calculate the tip for the bill.*/
+        // Calculates the balance due to the student upon finishing their degree
         document.querySelector('#calculate').onclick = calculateCost;
     }
   
@@ -12,7 +11,7 @@ function calculateCost() {
     let yearly_cost = document.querySelector('#yearly-cost').value;
     let total_years = document.querySelector('#total-years').value;
     let hours_worked = document.querySelector('#hours-worked').value;
-    let semester_weeks = 15;
+    // let semester_weeks = 15;
   
     console.log(hours_worked);
     /*if statement will work when user presses 
@@ -29,17 +28,17 @@ function calculateCost() {
     // }
   
     //now we are checking number of total_years 
-    if (total_years === '1')
-    //if there is only one total_years then we need not to display each.
-        document.querySelector('#each').style.display = 'none';
-    else
-    //if there are more than one total_years we will display each.  
-        document.querySelector('#each').style.display = 'block';
+    // if (total_years === '1')
+    // //if there is only one total_years then we need not to display each.
+    //     document.querySelector('#each').style.display = 'none';
+    // else
+    // //if there are more than one total_years we will display each.  
+    //     document.querySelector('#each').style.display = 'block';
   
     /*calculating the tip by multiplying total-bill and number of
      hours_worked; then dividing it by number of total_years.*/
     //fixing the total yearly_cost upto 2 digits of decimal
-    let total = yearly_cost / total_years;
+    let total = yearly_cost * total_years;
     // let total = yearly_cost - (semester_weeks * hours_worked) / total_years;
     // component that includes scholarships
 
