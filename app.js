2f87bebeb4375a3c9cd714f3914f6c7ecbfafcc5
mfturra <matheus.turra.b@gmail.com>
2023-10-17 14:02:22 -0400

    Troubleshooting calculations with addition of hidden features

--- app.js
+++ app.js
@@ -1,40 +1,45 @@
 // Holistic calculate function
-// window.onload = () =>
+// document.addEventListener('DOMContentLoaded', () => {
+//     const loanTypeSelection = document.getElementById('loan_type_selection');
+//     const undecidedLoanType = document.getElementById('hidden_loan_type_undecided');
+//     const fixedLoanType =     document.getElementById('hidden_loan_type_fixed');
+//     const variableLoanType =  document.getElementById('hidden_loan_type_variable')
 
-document.addEventListener('DOMContentLoaded', () => {
-    const loanTypeSelection = document.getElementById('loan_type_selection');
-    const undecidedLoanType = document.getElementById('hidden_loan_type_undecided');
-    const fixedLoanType =     document.getElementById('hidden_loan_type_fixed');
-    const variableLoanType =  document.getElementById('hidden_loan_type_variable')
-
-    loanTypeSelection.addEventListener('change', function handleChange(event) {
-        // Get selected value
-        const selectedValue = event.target.value;
+//     loanTypeSelection.addEventListener('change', function handleChange(event) {
+//         // Get selected value
+//         const selectedValue = event.target.value;
         
-        // Hide all loan type sections
-        undecidedLoanType.style.display =    'none';
-        fixedLoanType.style.display =        'none';
-        variableLoanType.style.display =     'none';
+//         // Hide all loan type sections
+//         undecidedLoanType.style.display =    'none';
+//         fixedLoanType.style.display =        'none';
+//         variableLoanType.style.display =     'none';
 
-        // Reveal the selected loan type section
-        if (selectedValue === '0') {
-            undecidedLoanType.style.display = 'block';
-        } else if (selectedValue === '1') {
-            fixedLoanType.style.display = 'block';
-        } else if (selectedValue === '2') {
-            variableLoanType.style.display = 'block';
-        }
+//         // Reveal the selected loan type section
+//         if (selectedValue === '0') {
+//             undecidedLoanType.style.display = 'block';
+//         } else if (selectedValue === '1') {
+//             fixedLoanType.style.display = 'block';
+//         } else if (selectedValue === '2') {
+//             variableLoanType.style.display = 'block';
+//         }
 
-    });
+//     });
+// });
 
-
-    // Calculates the balance due to the student upon finishing their degree
+window.onload = () => {
+    // Acquire users general tuition cost
     document.querySelector('#yearly-cost').oninput = calculateCost;
     document.querySelector('#total-years').oninput = calculateCost;
+
+    // Acquire users projected loan costs
+    document.querySelector('#loan-interest-value').oninput = calculateCost;
+    document.querySelector('#loan-cost').oninput = calculateCost;
+    
+    // Acquire users expected income
     document.querySelector('#num-semesters').oninput = calculateCost;
     document.querySelector('#hours-worked').oninput = calculateCost;
     document.querySelector('#hourly-rate').oninput = calculateCost;
-});
+}
   
 
 // function showDiv(divId, element) {
@@ -48,17 +53,12 @@ document.addEventListener('DOMContentLoaded', () => {
 //     // document.querySelectorAll('#hidden_loan_type_description > div').forEach(section => {
 //     //     section.style.display = 'none';
 //     // });
-
-//     // Show selected section
-//     undecidedLoanType.style.display =   element.value == "0" ? 'block' : 'none';
-//     fixedLoanType.style.display =       element.value == "1" ? 'block' : 'none';
-//     variableLoanType.style.display =    element.value == "2" ? 'block' : 'none';
 // }
 
 
 
 function calculateCost() {
-    var yearly_cost =   document.getElementById('yearly-cost').value;
+    var yearly_cost =   Number(document.getElementById('yearly-cost').value);
     var total_years =   Number(document.getElementById('total-years').value);
     var num_semesters = Number(document.getElementById('num-semesters').value);
     var hours_worked =  Number(document.getElementById('hours-worked').value);
@@ -67,6 +67,7 @@ function calculateCost() {
 
     // Calculations
     var total_tuition_cost = yearly_cost * total_years;
+    var total_loan_expenses = total_tuition_cost * loan_interest;
     var expected_weekly_earnings = hours_worked * hourly_rate;
     var expected_semester_earnings = expected_weekly_earnings * avg_weeks_in_semesters; // average number of weeks in semester
     var expected_degree_earnings = expected_semester_earnings * num_semesters;
@@ -74,13 +75,14 @@ function calculateCost() {
 
     // Log users calculation input to console
     console.log(total_tuition_cost)
+    console.log(total_loan_expenses)
     console.log(expected_weekly_earnings)
     console.log(expected_semester_earnings)
     console.log(expected_degree_earnings)
 
 
-    // Show Total Cost of Tuition
-    document.getElementById('outstanding-balance').style.display = 'block';
+    // Show General Cost of Tuition
+    document.getElementById('general-tuition-cost').style.display = 'block';
     document.querySelector('#total_tuition_cost').innerHTML = total_tuition_cost;
 
     // Show Income Generated
