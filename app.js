57472117be0cb54112ea554632b9623a02afc31f
mfturra <matheus.turra.b@gmail.com>
2023-10-26 11:21:16 -0400

    Added preliminary tab component to webpage

--- app.js
+++ app.js
@@ -38,7 +38,26 @@ document.addEventListener('DOMContentLoaded', () => {
             variableLoanType.style.display = 'block';
         }
 })});
-        
+
+function openTab (event, tabName) {
+    var i, tabcontent, tablinks;
+
+    // Hide all element with class='tabcontent'
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show current tab and add "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+
+}
 
 function calculateCost() {
     var yearly_cost =   Number(document.getElementById('yearly-cost').value);
