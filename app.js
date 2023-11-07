6fb1276ddb311ba63f64673d834cd92618b27d0b
mfturra <matheus.turra.b@gmail.com>
2023-11-07 14:01:25 -0500

    Research tabs workflow was built to allow users to take multiple research paths

--- app.js
+++ app.js
@@ -51,6 +51,26 @@ function openTab (event, tabName) {
         tabcontent[i].style.display = "none";
     }
 
+    tablinks = document.getElementsByClassName("researchlinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show current tab and add "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+
+}
+
+function researchingTabs (event, tabName) {
+    var i, tabcontent, tablinks;
+    
+    // Hide all element with class='tabcontent'
+    tabcontent = document.getElementsByClassName("research-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
@@ -61,6 +81,7 @@ function openTab (event, tabName) {
     event.currentTarget.className += " active";
 
 }
+
 
 function calculateCost() {
     var yearly_cost =   Number(document.getElementById('yearly-cost').value);
